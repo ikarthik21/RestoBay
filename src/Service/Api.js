5d0580c4fdd7ac1d84d3de26673349e58102e27b
ikarthik21 <karthikeyakallapu@gmail.com>
2023-07-22 16:30:08 +0530

    fixes

--- src/Service/Api.js
+++ src/Service/Api.js
@@ -11,8 +11,8 @@ if (token) {
 }
 
 const curr_url = process.env.REACT_APP_BACKEND_URL;
- 
 
+console.log(curr_url)
 export const adduser = async (user) => {
    try {
       return axios.post(`${curr_url}/register`, user);
